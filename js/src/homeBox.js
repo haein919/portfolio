5c2b99eda4ecc16c8f26db9da049c549691bd134
haein919 <haein919@gmail.com>
2020-12-28 19:25:44 +0900

    parallax modify

--- js/src/homeBox.js
+++ js/src/homeBox.js
@@ -4,17 +4,26 @@
   var win = $(window) ;
   var winH = win.outerHeight();
 
+  $('html, body').animate({scrollTop:0});
+
   var homeBox = $('#homeBox');
   var homeBoxHeight = homeBox.outerHeight();
   var aboutBox = $('#aboutBox');
+  aboutBox.css({paddingTop:winH*3.5+'px'});
   var aboutScroll =aboutBox.offset().top;
-  console.log(aboutScroll);
+  // console.log(aboutScroll);
 
   var homeText = homeBox.find('.context_area')
   var moonImg = homeBox.find('.moon_img');
   
 
   // 브라우저 스크롤시 수행
+  var MoonFn = function(setScroll){
+    var sscroll = setScroll || 0 
+    return 95-(sscroll)*5;
+  };
+  MoonFn();
+
   win.on('scroll', function(){
     var winSt = win.scrollTop();
     var setScroll = winSt /100;
@@ -22,20 +31,36 @@
 
     var i =0;
   // 달 나타나면서 올라가기
-    if( winSt < 969){
-      moonImg.css({'background-positionY':  (95-(setScroll)*5) + '%'})
-    }
-
-    if( 969< winSt < 1500 ){
-      var opacityR = i + 1 - per;
+  
+  // MoonFn(setScroll);
+  // console.log( MoonFn(setScroll) );
+  var homeBoxOp;
+  var opacityR;
+  homeBox.css({opacity: 1});
+  homeBoxOp =  1+MoonFn(setScroll)/50;
+    // console.log('homeBoxOp: ', 1-homeBoxOp);
+    if(MoonFn(setScroll) <= 100 && MoonFn(setScroll) > 50){      
+      moonImg.css({'background-positionY':  MoonFn(setScroll) + '%'});
+    }else if(MoonFn(setScroll) > 0 && MoonFn(setScroll) <= 50){
+      opacityR = i + 1 - per;
       homeText.css({'opacity':-opacityR});
+    }else{
+      console.log('!!!');
+      // homeBoxOp =  MoonFn(setScroll)/50;
+      homeBox.css({opacity:homeBoxOp});
+      console.log( homeBoxOp );
     }
 
-    if(winSt > 1900){
-      homeBox.css({'position':'static'});
-    }else{
-      homeBox.removeAttr('style');
-    }
+    // if( 969 < winSt < 1500 ){
+    //   var opacityR = i + 1 - per;
+    //   homeText.css({'opacity':-opacityR});
+    // }
+
+    // if(winSt > 1900){
+    //   // homeBox.css({'position':'static'});
+    // }else{
+    //   homeBox.removeAttr('style');
+    // }
   });
 
  // jQuery end
